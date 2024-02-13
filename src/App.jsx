4fd9e8be27dce6507d3c01c4f647d663585bbfa9
Kris Ly <chily2808@gmail.com>
2024-02-12 23:40:41 -0600

    Add dark-light-mode p2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,66 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors';
-import Typography from '@mui/material/Typography';
-import {  useColorScheme } from '@mui/material/styles';
+import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
+import {  useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode)
+  };                                                                                         
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{display:'flex', alignItems:'center', gap: '8px'}}>
+            <LightModeIcon fontSize='small'/> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{display:'flex', alignItems:'center', gap: '8px'}}>
+            <DarkModeOutlinedIcon fontSize='small'/> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{display:'flex', alignItems:'center', gap: '8px'}}>
+            <SettingsBrightnessIcon fontSize='small'/> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
+
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
+  // console.log('prefersDarkMode',prefersDarkMode)
+  // console.log('prefersLightMode',prefersLightMode)
+
   return (
     <Button
       onClick={() => {
@@ -22,6 +76,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect/>
+      <hr></hr>
       <ModeToggle/>
       <hr></hr>
       <div>Krisly8386</div>
