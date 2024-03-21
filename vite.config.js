a9a5607ad0b7a7112d6ff7ca8fe70e80b5c2b41e
Kris Ly <chily2808@gmail.com>
2024-03-20 22:57:34 -0500

    fixed build mode

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define:{
+  // Cho phép thằng Vite sử dụng được process.env, mặc định thì không mà sẽ phải dùng import.meta.env
+  // https://github.com/vitejs/vite/issues/1973
+  define: {
     'process.env': process.env
   },
   plugins: [
