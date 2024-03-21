80b82098ea68159590725bdb2e88436077867808
Kris Ly <chily2808@gmail.com>
2024-03-20 22:53:23 -0500

    fixed build mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ if (process.env.BUILD_MODE === 'dev') {
 // Môi trường Production sẽ cần api endpoint chuẩn của các bạn
 if (process.env.BUILD_MODE === 'production') {
   // Lưu ý: Đây là domain ví dụ sau khi Deploy Production (xem video 75 và video 76 để hiểu rõ kiến thức phần này, còn hiện tại mình đã xóa domain này rồi, đừng cố truy cập làm gì =))
-  apiRoot = 'https://trello-api-0gbu.onrender.com'
+  apiRoot = 'https://trello-api-sw51.onrender.com'
 }
 // console.log('🚀 ~ file: constants.js:7 ~ apiRoot:', apiRoot)
 export const API_ROOT = apiRoot
